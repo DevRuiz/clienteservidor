1ecb10b873e80ca2c608daa650f8a68d06be688d
Fernanda Ruiz <ferruiz200019@hotmail.com>
2020-02-10 11:46:55 -0600

    metodo post y get

--- CS/settings.py
+++ CS/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'Login'
 ]
 
 SITE_ID = 1
